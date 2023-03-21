23ca7e7ecd8985c2653c94e84a9ec5b53d120643
Stefan Mars <stefanrmars@gmail.com>
2023-03-21 11:26:47 +0100

    Add project files.

---
+++ Desktop App/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace Desktop_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.PictureBox2 = new System.Windows.Forms.PictureBox();
+            this.UpgradeG = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.UpgradeF = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Timer1
+            // 
+            this.Timer1.Enabled = true;
+            this.Timer1.Interval = 1000;
+            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(337, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "0";
+            // 
+            // PictureBox2
+            // 
+            this.PictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox2.Image")));
+            this.PictureBox2.Location = new System.Drawing.Point(178, 12);
+            this.PictureBox2.Name = "PictureBox2";
+            this.PictureBox2.Size = new System.Drawing.Size(97, 148);
+            this.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox2.TabIndex = 13;
+            this.PictureBox2.TabStop = false;
+            this.PictureBox2.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
+            // UpgradeG
+            // 
+            this.UpgradeG.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.UpgradeG.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.UpgradeG.Location = new System.Drawing.Point(12, 73);
+            this.UpgradeG.Name = "UpgradeG";
+            this.UpgradeG.Size = new System.Drawing.Size(75, 23);
+            this.UpgradeG.TabIndex = 14;
+            this.UpgradeG.Text = "Grandma";
+            this.UpgradeG.UseVisualStyleBackColor = true;
+            this.UpgradeG.Click += new System.EventHandler(this.UpgradeG_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(92, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "10";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(139, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(93, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 17);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Price";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(139, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 17);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "LVL";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(280, 29);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 17);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "CPS";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(337, 29);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(16, 17);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(139, 108);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(16, 17);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "0";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(92, 108);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(32, 17);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "100";
+            // 
+            // UpgradeF
+            // 
+            this.UpgradeF.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.UpgradeF.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.UpgradeF.Location = new System.Drawing.Point(12, 102);
+            this.UpgradeF.Name = "UpgradeF";
+            this.UpgradeF.Size = new System.Drawing.Size(75, 23);
+            this.UpgradeF.TabIndex = 21;
+            this.UpgradeF.Text = "Farm";
+            this.UpgradeF.UseVisualStyleBackColor = true;
+            this.UpgradeF.Click += new System.EventHandler(this.UpgradeF_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "5";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(12, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Click";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.UpgradeC_Click_2);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1228, 357);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 28;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(1300, 357);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 17);
+            this.label13.TabIndex = 29;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 233);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 30;
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(33, 281);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 31;
+            this.button2.Text = "Check";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(280, 12);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(58, 17);
+            this.label15.TabIndex = 33;
+            this.label15.Text = "Cookies";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(17, 9);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(70, 17);
+            this.label16.TabIndex = 35;
+            this.label16.Text = "Upgrades";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(695, 598);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.UpgradeF);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.UpgradeG);
+            this.Controls.Add(this.PictureBox2);
+            this.Controls.Add(this.label4);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer Timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox PictureBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button UpgradeG;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button UpgradeF;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+    }
+}
+
---
+++ Desktop App/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using MySql.Data.MySqlClient;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Desktop_App
+{
+    public partial class Form1 : Form
+    {
+        int cookieClick = 1;
+        int Cprice = 5;
+        int cookies = 0;
+
+        int Gprice = 10;
+        int Grandma = 0;
+
+        int Fprice = 100;
+        int Farm = 0;
+
+        int CPS = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            label1.Text = cookieClick.ToString();
+            try
+            {
+                string connstring = "server=localhost;uid=root;pwd=;database=dbs10041809;";
+                //string connstring = "server=rdbms.strato.de;uid=dbu2577448;pwd=KabouterST06;database=dbs10041809;";
+                MySqlConnection conn = new MySqlConnection
+                {
+                    ConnectionString = connstring
+                };
+                conn.Open();
+                string sql = "SELECT * FROM users";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                label3.Text = "username" + Environment.NewLine;
+                label13.Text = "password" + Environment.NewLine;
+                while (reader.Read())
+                {
+                    
+                    label3.Text += reader["username"] + Environment.NewLine;
+                    label13.Text += reader["password"] + Environment.NewLine;
+                }
+            }catch(MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            cookies += cookieClick;
+            label4.Text = cookies.ToString();
+            //pictureBox2.Size = new Size(275, 275);
+            //pictureBox2.Size = new Size(300, 300);
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            cookies = CPS + cookies;
+            label4.Text = cookies.ToString();
+        }
+        public void Cupdate()
+        {
+            label10.Text = CPS.ToString();
+            label4.Text = cookies.ToString();
+        }
+        private void UpgradeC_Click_2(object sender, EventArgs e)
+        {
+            if (cookies >= Cprice)
+            {
+                cookies -= Cprice;
+                cookieClick++;
+                Cprice += 5;
+                label2.Text = Cprice.ToString();
+                label1.Text = cookieClick.ToString();
+                Cupdate();
+            }
+        }
+        private void UpgradeG_Click(object sender, EventArgs e)
+        {
+            if (cookies >= Gprice)
+            {
+                cookies -= Gprice;
+                Grandma++;
+                Gprice += 10;
+                CPS++;
+                label5.Text = Gprice.ToString();
+                label6.Text = Grandma.ToString();
+                Cupdate();
+            }
+        }
+
+        private void UpgradeF_Click_1(object sender, EventArgs e)
+        {
+            if (cookies >= Fprice)
+            {
+                cookies -= Fprice;
+                Farm++;
+                Fprice += 50;
+                CPS += 5;
+                label12.Text = Fprice.ToString();
+                label11.Text = Farm.ToString();
+                Cupdate();
+            }
+                
+
+        }
+
+        
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //if (textBox1 == reader["password"])
+            //{
+
+            //}
+        }
+    }
+}
+
+
+
+
